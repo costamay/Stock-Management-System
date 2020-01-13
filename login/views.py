e90b30813b316874a60897eea603f1d9b838ff7d
Albert Byrone <albertbyrone1677@gmail.com>
2020-01-13 20:22:14 +0300

    import the required modules in the login views

--- login/views.py
+++ login/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse,HttpResponseRedirect
+
+from django.contrib.auth import login,authenticate
+from login.models import *
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import REDIRECT_FIELD_NAME
+
 
 # Create your views here.
