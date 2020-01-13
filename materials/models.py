57998fbcd95727652e70504ca3e64fa608f5e01c
Tyra-hans <tyrahans17@gmail.com>
2020-01-13 19:59:12 +0300

    Adds supplier and materials tables with appropriate fileds and methods for saving and deleting

--- materials/models.py
+++ materials/models.py
@@ -1,2 +1,27 @@
 from django.db import models
 
+class Material(models.Model):
+    m_name = models.CharField(max_length=100)
+    qyt = models.FloatField()
+    price = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def save_material(self):
+        self.save()
+
+    def delete_material(self):
+        self.delete()
+
+    @classmethod   
+    def update_supplier(cls,id,new_name):
+        cls.objects.filter(pk = id).update(m_name=new_name)
+        new_name_object = cls.objects.get(m_name = new_name)
+        new_name = new_name_object.name
+        return new_name
+
+    def __str__(self):
+        return f'{self.m_name}'
+
+    class Meta:
+        ordering = ['date']
+
