9e0a2410cb920358105a7bbe71dd77c6e37777f0
Titus Ouko <titusouko@gmail.com>
2020-01-13 19:30:59 +0300

    Add view function for suppliers app

--- supplier/views.py
+++ supplier/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+from .models import *
+
+def all_suppliers(request):
+    suppliers = Supplier.objects.all()
+   
+    return render(request, 'suppliers.html', locals())
