5ed0c1bb9d433fe200bdc09e1cd02c1c83004668
Albert Byrone <albertbyrone1677@gmail.com>
2020-01-13 20:23:06 +0300

    create a login view

--- login/views.py
+++ login/views.py
@@ -7,5 +7,24 @@ from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import REDIRECT_FIELD_NAME
 
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get("passowrd")
+        user = authenticate(request,email=email,password=password)
+
+        if user is not None:
+            login(request,user)
+            typ_obj = user_type.objects.get(user=user)
+
+            if user.is_authenticated and typ_obj.is_accountant:
+                return redirect('achome')
+            elif user.is_authenticated and typ_obj.is_manager:
+                return redirect('shome')
+            elif user.is_authenticated and typ_obj.is_staff:
+                return redirect('ahome')
+        else:
+            return redirect('home')
+    return render(request,'home.html')
 
 # Create your views here.
