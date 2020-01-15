ecc420cedba803298d67cd911a5938acf4dd0df5
Albert Byrone <albertbyrone1677@gmail.com>
2020-01-15 13:17:42 +0300

    update the product urls

--- products/urls.py
+++ products/urls.py
@@ -5,11 +5,12 @@ from products.views import *
 from . import views
 
 urlpatterns = [
-
-    url(r'^product', views.product,name="product"),   
-    url(r'^edit-product/(?P<id>\d+)', views.edit_product,name="edit-product"),  
-    url(r'^update-product/(?P<id>\d+)', views.update_product,name="edit-product"),  
-    url(r'^delete-product/(?P<id>\d+)', views.delete_product,name="edit-product"),  
+    
+    url(r'^products/$', views.product_form,name="add_product"), 
+    url(r'^products/(?P<id>\d+)/$', views.product_form,name="update_product"),
+    url(r'^products/delete/(?P<id>\d+)/$', views.delete_product,name="delete_product"),
+    url(r'^products/list/$',views.product_list,name="product_list"),
+     
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
