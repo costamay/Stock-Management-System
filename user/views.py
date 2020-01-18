bc38ef521836ac913fc8e71c4275a69d97a95df7
Albert Byrone <albertbyrone1677@gmail.com>
2020-01-18 13:03:47 +0300

    minor changes

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,HttpResponse
-from products.forms import *
+from users.forms import *
 
 def product_list(request):
     products =  Product.objects.all()
