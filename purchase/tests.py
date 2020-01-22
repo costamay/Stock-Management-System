3c9898c50f3d9d31b07db06d706cf3dcd57149d1
Tyra-hans <tyrahans17@gmail.com>
2020-01-22 17:04:13 +0300

    writes tests for seting up and is_instance for supplier and purchase

--- purchase/tests.py
+++ purchase/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Purchase
 
-# Create your tests here.
+class PurchaseTestClass(TestCase):
+    # setup method
+    def setUp(self):
+        self.purchase1 = Purchase(pu_date = '01-01-2020', qyt = 200.01 , price = 90.12 )
+
+        # testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.purchase1,Purchase))
+
+    # saving instance
+
+    # def test_save_method(self):
+    #     self.purchase1.save_purchase()
+    #     purchase1 = Purchase.objects.all()
+    #     self.assertTrue(len(purchase1) > 0)
--- supplier/tests.py
+++ supplier/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Supplier
 
-# Create your tests here.
+class SupplierTestClass(TestCase):
+    # setup method
+    def setUp(self):
+        self.supplier1 = Supplier(s_name = 'Farmer', s_phone = '0712345678' , date = '01-01-2020' )
+
+        # testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.supplier1,Supplier))
+
