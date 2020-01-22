52cf573911f4a3fe509bbe17941b14abf4bc0972
Tyra-hans <tyrahans17@gmail.com>
2020-01-22 17:07:48 +0300

    Adds set up and is_instance tests for materials model

--- materials/tests.py
+++ materials/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Material
 
-# Create your tests here.
+class MaterialTestClass(TestCase):
+    # setup method
+    def setUp(self):
+        self.material1 = Material(m_name = 'onion', qyt = 10.00 , price = 100.98 ,date = '01-12-2020' )
+
+        # testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.material1,Material))
