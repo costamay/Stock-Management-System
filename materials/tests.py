b9ea8bcd3cd5f1517446e8185623402aa76347d1
Tyra-hans <tyrahans17@gmail.com>
2020-01-23 11:09:28 +0300

    Writes tests for saving and deleting methods in the material table

--- materials/tests.py
+++ materials/tests.py
@@ -4,8 +4,28 @@ from .models import Material
 class MaterialTestClass(TestCase):
     # setup method
     def setUp(self):
-        self.material1 = Material(m_name = 'onion', qyt = 10.00 , price = 100.98 ,date = '01-12-2020' )
+        self.material1 = Material(m_name = 'onion',
+        qyt = 10.00 ,
+        price = 100.98 ,
+        date = '01-12-2020' )
 
         # testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.material1,Material))
+
+        # saving method
+    def test_save_method(self):
+        self.material1.save_material()
+        material1 = Material.objects.all()
+        self.assertTrue(len(material1) > 0)
+
+        #tear down method
+    def tearDown(self):
+        Material.objects.all().delete()
+
+        # delete method
+    def test_delete_method(self):
+        self.material1.save_material()
+        self.material1.delete_material()
+        material1 = Material.objects.all()
+        self.assertTrue(len(material1)== 0 )
