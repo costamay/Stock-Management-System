d3e17020e6c3c512a2e18a775ea541d90fc8d98a
Tyra-hans <tyrahans17@gmail.com>
2020-01-23 12:06:06 +0300

    Adds tests for client model

--- sales/tests.py
+++ sales/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Sale
+from client.models import Client
+from products.models import Product
+
+class SaleTestClass(TestCase):
+    # setup method
+    def setUp(self):
+        self.sale1 = Sale(client = Client.objects.create(c_name = 'Naivas', c_phone = 11 , date = '10-10-2020', 
+        product = Product.objects.create(p_name = 'someproduct',
+        p_image = 'image.jpg' ,
+        size = '120g' ,
+        qyt = 22,
+        price = 20.20,
+        category = 'mild',
+        date = '12-12-2020')),
+        date = '12-12-2020'
+        ),
+
+    # testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sale1,Sale))
+   
