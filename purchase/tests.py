b17ccaf598c5df011ebc86f79fa1a21b5b8af952
Tyra-hans <tyrahans17@gmail.com>
2020-01-23 09:09:18 +0300

    writes tests for saving the saving methods in the purchase model

--- purchase/tests.py
+++ purchase/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
+from django.utils import timezone
 from .models import Purchase
+from materials.models import Material
+from supplier.models import Supplier
 
 class PurchaseTestClass(TestCase):
     # setup method
     def setUp(self):
-        self.purchase1 = Purchase(pu_date = '01-01-2020', qyt = 200.01 , price = 90.12 )
+        self.purchase1 = Purchase(pu_date = '01-01-2020',
+        qyt = 200.01 ,
+        price = 90.12 ,
+        material = Material.objects.create(m_name = 'onion', qyt = 2.0 , price = 200.0 , date = '01-01-2020'),
+        supplier = Supplier.objects.create(s_name = 'farmer' , s_phone = '0712345678', date = '10-10-2020' ,
+        materials = Material.objects.create(m_name = 'onion', qyt = 2.0 , price = 200.0 , date = '01-01-2020')))
 
         # testing instance
     def test_instance(self):
@@ -12,7 +20,7 @@ class PurchaseTestClass(TestCase):
 
     # saving instance
 
-    # def test_save_method(self):
-    #     self.purchase1.save_purchase()
-    #     purchase1 = Purchase.objects.all()
-    #     self.assertTrue(len(purchase1) > 0)
+    def test_save_method(self):
+        self.purchase1.save_purchase()
+        purchase1 = Purchase.objects.all()
+        self.assertTrue(len(purchase1) > 0)
