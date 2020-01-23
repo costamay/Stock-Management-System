e94039b1beefc8178bbb8e62a3aeb809c1a62208
Tyra-hans <tyrahans17@gmail.com>
2020-01-23 10:54:42 +0300

    Supplier save and delete method passes tests

--- supplier/tests.py
+++ supplier/tests.py
@@ -10,3 +10,24 @@ class SupplierTestClass(TestCase):
         s_phone = '0712345678' ,
         date = '20-12-2020' ,
         materials = Material.objects.create(m_name = 'onion', qyt = 2.0 , price = 200.0 , date = '01-01-2020'))
+
+      # testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.supplier1,Supplier))
+
+       # saving method
+    def test_save_method(self):
+        self.supplier1.save_supplier()
+        supplier1 = Supplier.objects.all()
+        self.assertTrue(len(supplier1) > 0)
+
+      #tear down method
+    def tearDown(self):
+        Supplier.objects.all().delete()
+
+    # delete method
+    def test_delete_method(self):
+        self.supplier1.save_supplier()
+        self.supplier1.delete_supplier()
+        supplier1 = Supplier.objects.all()
+        self.assertTrue(len(supplier1)== 0 )
