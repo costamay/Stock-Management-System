e84b37b87745b51c570a22ec9b8e7be1206f27f7
Tyra-hans <tyrahans17@gmail.com>
2020-01-23 11:00:41 +0300

    writes Setup method for products test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+class ProductTestClass(TestCase):
+    # setup method
+    def setUp(self):
+        self.product1 = Product(p_name = 'someproduct',
+        p_imge = 'image.jpg' ,
+        size = '120g' ,
+        qyt = 22,
+        price = 20.20,
+        category = 'mild',
+        date = '12-12-2020')
